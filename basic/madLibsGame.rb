b180bbae12c0ef726853eb8f3fafc6625338ccaa
nishant-ranjan28 <nishantranjan78@gmail.com>
2020-02-07 22:30:43 +0530

    madLibsGame

---
+++ basic/madLibsGame.rb
@@ -0,0 +1,16 @@
+puts "Enter your name"
+name = gets.chomp()
+
+puts "Enter a color"
+color = gets.chomp()
+
+puts "Enter a celebrity name"
+celebrity = gets.chomp()
+
+puts "Enter a sports"
+sports = gets.chomp()
+
+puts ("your name is " + name)
+puts ("your favorite color is " + color)
+puts ("Your favorite celebrity is " + celebrity)
+puts ("Your favorite sports is " + sports)
