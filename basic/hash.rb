2a251ae0daa4c0d223ac6c3204f24a22009f974b
nishant-ranjan28 <nishantranjan78@gmail.com>
2020-02-11 15:03:39 +0530

    hash codes

---
+++ basic/hash.rb
@@ -0,0 +1,11 @@
+states = {
+    "Hyderabad" => "HYD",
+    "Bangalore" => "BLR",
+    "Patna" => "PAT",
+    :Chennai => "CHE",
+    1 => "num"
+}
+
+#puts states["Patna"]
+#puts states[:Chennai]
+puts states[1]
