d3100f241811e60243f874a6c2a67970ef7571b2
nishant-ranjan28 <nishantranjan78@gmail.com>
2020-03-07 17:10:44 +0530

    modified code

--- basic/class.rb
+++ basic/class.rb
@@ -10,12 +10,18 @@ book1.title = "Harry Potter"
 book1.author = "JK Rowling"
 book1.pages = 400
 
+puts book1.title
+puts book1.author
 puts book1.pages
+
+#second method
 
 book2 = Book.new()
 
-book2.author = "Anyone"
+book2.author = "Tolkein"
 book2.pages = 300
 book2.title = "Lord of the Ring"
 
 puts book2.title
+puts book2.author
+puts book2.pages
--- basic/initialize_methods.rb
+++ basic/initialize_methods.rb
@@ -1,17 +1,23 @@
 #class is a customized data type in ruby.
 
 class Book
-    attr_accessor :title, :author, :pages
-    def initialize(title, author, pages)
-        @title = title
-        @author = author
-        @pages = pages
-    end
+  attr_accessor :title, :author, :pages
+
+  def initialize(title, author, pages)
+    @title = title
+    @author = author
+    @pages = pages
   end
-  
-  book1 = Book.new("Harry Potter","JK Rowling", 400)
-  
-  book2 = Book.new("Lord of the Ring", "Tolkein", 500)
-  
-  puts book2.pages
-  puts book1.title
+end
+
+book1 = Book.new("Harry Potter", "JK Rowling", 400)
+
+book2 = Book.new("Lord of the Ring", "Tolkein", 500)
+
+puts book1.title
+puts book1.author
+puts book1.pages
+
+puts book2.title
+puts book2.author
+puts book2.pages
